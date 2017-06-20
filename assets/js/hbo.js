83366b6fe8e4390ff3b1db8f2b6da244ef77b15b
dh-dev-prog <Damien@MacBook-Pro-de-Damien.local>
2017-06-20 21:38:02 +0900

    Add toggle to header icons

--- assets/js/hbo.js
+++ assets/js/hbo.js
@@ -1,11 +1,45 @@
 function initPage(){
+
   var iconNav = document.getElementById('navIcon');
+  var iconSearch = document.getElementById('searchIconMix');
+
+  function navToggle() {
+    iconNav.classList.toggle('ion-navicon');
+    iconNav.classList.toggle('ion-ios-close-empty');
+    document.getElementById('menu').classList.toggle('is_active_block');
+  }
+  function navClose() {
+    iconNav.classList.remove('ion-ios-close-empty');
+    iconNav.classList.add('ion-navicon');
+    document.getElementById('menu').classList.add('is_hidden');
+    document.getElementById('menu').classList.remove('is_active_block');
+  }
+  function searchToggle(){
+    document.getElementById('searchIcon').classList.toggle('ion-ios-search-strong');
+    document.getElementById('searchCircle').classList.toggle('ion-ios-circle-outline');
+    document.getElementById('searchIcon').classList.toggle('ion-ios-close-empty');
+    document.getElementById('searchBar').classList.toggle('is_hidden');
+  }
+  function searchClose() {
+    var searchBar = document.getElementById('searchBar');
+    searchIcon.classList.remove('ion-ios-close-empty');
+    document.getElementById('searchIcon').classList.add('ion-ios-search-strong');
+    document.getElementById('searchCircle').classList.add('ion-ios-circle-outline');
+    searchBar.className = 'search' + ' ' + 'is_hidden';
+
+    iconSearch.firstChild.className = 'ion-ios-search-strong';
+    iconSearch.lastChild.className = 'ion-ios-circle-outline';
+
+  }
 
   iconNav.addEventListener('click', function(){
-    this.classList.toggle('ion-navicon');
-    document.getElementById('dropdown').classList.toggle('is_active_block')
-    this.classList.toggle('ion-ios-close-empty');
-  })
+    searchClose()
+    navToggle();
+  });
+  iconSearch.addEventListener('click', function(){
+    navClose();
+    searchToggle();
+  });
 }
 
 initPage();
