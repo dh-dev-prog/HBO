67fd610a00a41ff6b587dfb3ef62207be4dc7237
dh-dev-prog <Damien@MacBook-Pro-de-Damien.local>
2017-06-21 06:54:41 +0900

    Clean JS - Toggle header icons

--- assets/js/hbo.js
+++ assets/js/hbo.js
@@ -1,42 +1,40 @@
 function initPage(){
 
   var iconNav = document.getElementById('navIcon');
-  var iconSearch = document.getElementById('searchIconMix');
+  var iconSearch = document.getElementById('searchIcon');
+  var iconCircle = document.getElementById('searchCircle');
+
+  var menu = document.getElementById('menu');
+  var search = document.getElementById('searchBar');
 
   function navToggle() {
     iconNav.classList.toggle('ion-navicon');
     iconNav.classList.toggle('ion-ios-close-empty');
-    document.getElementById('menu').classList.toggle('is_active_block');
+    menu.classList.toggle('is_hidden');
   }
   function navClose() {
     iconNav.classList.remove('ion-ios-close-empty');
     iconNav.classList.add('ion-navicon');
-    document.getElementById('menu').classList.add('is_hidden');
-    document.getElementById('menu').classList.remove('is_active_block');
+    menu.classList.add('is_hidden');
   }
   function searchToggle(){
-    document.getElementById('searchIcon').classList.toggle('ion-ios-search-strong');
-    document.getElementById('searchCircle').classList.toggle('ion-ios-circle-outline');
-    document.getElementById('searchIcon').classList.toggle('ion-ios-close-empty');
-    document.getElementById('searchBar').classList.toggle('is_hidden');
+    iconSearch.classList.toggle('ion-ios-search-strong');
+    iconCircle.classList.toggle('ion-ios-circle-outline');
+    iconSearch.classList.toggle('ion-ios-close-empty');
+    search.classList.toggle('is_hidden');
   }
   function searchClose() {
-    var searchBar = document.getElementById('searchBar');
-    searchIcon.classList.remove('ion-ios-close-empty');
-    document.getElementById('searchIcon').classList.add('ion-ios-search-strong');
-    document.getElementById('searchCircle').classList.add('ion-ios-circle-outline');
-    searchBar.className = 'search' + ' ' + 'is_hidden';
-
-    iconSearch.firstChild.className = 'ion-ios-search-strong';
-    iconSearch.lastChild.className = 'ion-ios-circle-outline';
-
+    iconSearch.classList.remove('ion-ios-close-empty');
+    iconSearch.classList.add('ion-ios-search-strong');
+    iconCircle.classList.add('ion-ios-circle-outline');
+    search.classList.add('is_hidden');
   }
 
-  iconNav.addEventListener('click', function(){
+  iconNav.parentNode.addEventListener('click', function(){
     searchClose()
     navToggle();
   });
-  iconSearch.addEventListener('click', function(){
+  iconSearch.parentNode.addEventListener('click', function(){
     navClose();
     searchToggle();
   });
